a856ac2e32e313dc89d2e1f338ff7d2770b9ba91
sophiakopp <sophiakopp@gmx.de>
2025-10-16 17:13:14 +0200

    add hall drawing

--- frontend/src/CinemaHalls/HallCard/HallCard.tsx
+++ frontend/src/CinemaHalls/HallCard/HallCard.tsx
@@ -1,5 +1,6 @@
 import type {CinemaHall} from "../../model/CinemaHall.ts";
 import './HallCard.css';
+import Seats from "./Seats/Seats.tsx";
 
 type HallCardProps = {
     hall: CinemaHall
@@ -9,6 +10,16 @@ export default function HallCard(props: Readonly<HallCardProps>) {
         <div className={"hall-card"}>
             <h3>Id: {props.hall.id}</h3>
             <h3>Name: {props.hall.name}</h3>
+            <div className={"rows"}>
+                <hr/>
+                {[...Array(props.hall.rows)].map(() =>
+                    <div className={"seatsPerRow"}>
+                        {[...Array(props.hall.seatsPerRow)].map(() =>
+                            <Seats/>
+                        )}
+                    </div>
+                )}
+            </div>
         </div>
     )
 }
