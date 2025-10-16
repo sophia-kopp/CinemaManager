b9ac753b51af71e9ced8f6bc8dfb1d6e50748a10
sophiakopp <sophiakopp@gmx.de>
2025-10-16 11:33:48 +0200

    added tests for getAll in service and controller

---
+++ backend/src/test/java/org/example/backend/controller/HallControllerTest.java
@@ -0,0 +1,52 @@
+package org.example.backend.controller;
+
+import org.example.backend.model.CinemaHall;
+import org.example.backend.repo.HallRepo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class HallControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private HallRepo hallRepo;
+
+    @DirtiesContext
+    @Test
+    void getAllHalls_ShouldReturnListOfOneHall_WhenGetIsCalled() throws Exception {
+        //given
+        CinemaHall hall = new CinemaHall("1", "test", 4, 4);
+        hallRepo.save(hall);
+        //when
+        mockMvc.perform(get("/api/halls"))
+                //then
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(
+                        """     
+                                [
+                                {
+                                "id": "1",
+                                "name": "test",
+                                "rows": 4,
+                                "seatsPerRow":  4}
+                                ]
+                                """
+                ));
+    }
+
+    @Test
+    void addNewHall() {
+    }
+}
---
+++ backend/src/test/java/org/example/backend/service/HallServiceTest.java
@@ -0,0 +1,45 @@
+package org.example.backend.service;
+
+import org.example.backend.model.CinemaHall;
+import org.example.backend.repo.HallRepo;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+
+class HallServiceTest {
+    private HallRepo mockRepo;
+    private HallService hallService;
+
+    @BeforeEach
+    void setup() {
+        mockRepo = mock(HallRepo.class);
+        hallService = new HallService(mockRepo);
+    }
+
+    @Test
+    void getAllHalls_ShouldReturnListOfHalls_WhenServiceGetAllIsCalled() {
+        //GIVEN
+        CinemaHall hall1 = new CinemaHall("1", "test1", 4, 4);
+        CinemaHall hall2 = new CinemaHall("2", "test2", 8, 8);
+
+        List<CinemaHall> halls = List.of(hall1, hall2);
+
+        //WHEN
+        when(mockRepo.findAll()).thenReturn(halls);
+        List<CinemaHall> actual = hallService.getAllHalls();
+
+        //THEN
+        verify(mockRepo).findAll();
+        assertEquals(halls, actual);
+    }
+
+    @Test
+    void addNewHall() {
+    }
+}
