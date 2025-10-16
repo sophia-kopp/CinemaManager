e744c1821ec5527eb863eb9953cea228617168b1
sophiakopp <sophiakopp@gmx.de>
2025-10-16 13:55:52 +0200

    clean code in tests according to Sonar Cube in backend

--- backend/src/test/java/org/example/backend/service/IdServiceTest.java
+++ backend/src/test/java/org/example/backend/service/IdServiceTest.java
@@ -1,15 +1,11 @@
 package org.example.backend.service;
 
-import org.example.backend.repo.HallRepo;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
 
 class IdServiceTest {
 
-    HallRepo mockHallRepo = mock(HallRepo.class);
-    IdService idService = new IdService(mockHallRepo);
+    IdService idService = new IdService();
 
     @Test
     void randomIdTest_shouldGenerateRandomId(){
