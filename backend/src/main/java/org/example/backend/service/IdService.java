c05470fbabf9362206ba87ed20675a98c3ec40a4
sophiakopp <sophiakopp@gmx.de>
2025-10-16 13:54:23 +0200

    clean code according to Sonar Cube in backend

--- backend/src/main/java/org/example/backend/service/IdService.java
+++ backend/src/main/java/org/example/backend/service/IdService.java
@@ -1,17 +1,10 @@
 package org.example.backend.service;
 
-import org.example.backend.repo.HallRepo;
 import org.springframework.stereotype.Service;
-
 import java.util.UUID;
 
 @Service
 public class IdService {
-
-    private final HallRepo hallRepo;
-    public IdService(HallRepo hallRepo) {
-        this.hallRepo = hallRepo;
-    }
 
     public String generateUUid(){
         return UUID.randomUUID().toString();
