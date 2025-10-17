f165813104b7b031a92b921517066aede5e60d99
sophiakopp <sophiakopp@gmx.de>
2025-10-17 16:04:45 +0200

    ext movie db: made token enviroment variable

--- backend/src/main/java/org/example/backend/service/MovieService.java
+++ backend/src/main/java/org/example/backend/service/MovieService.java
@@ -6,11 +6,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.util.List;
 
 @Service
@@ -23,7 +18,7 @@ public class MovieService {
                 .baseUrl("https://api.themoviedb.org/3")
                 .requestInitializer(request -> {
                     // Set dynamic headers here
-                    String token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwYjVlMjhhMDdhYTgzMDQ3NWRiZjcwZmUzZTYxNDk1OSIsIm5iZiI6MTc2MDY5MzkyNy45MzcsInN1YiI6IjY4ZjIwZWE3ZGIwMjUyZTBjMTVhNTJkNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.TWNZzKGcTu2uSkMfJe4OE3ZwnaNYi3CyZ2fLf-kV6sE"; // Logic to fetch dynamic token
+                    String token =System.getenv("MOVIE_EXT_DB_TOKEN");
                     request.getHeaders().add(HttpHeaders.AUTHORIZATION, "Bearer " + token);
                     //request.getHeaders().add("Custom-Header", "SomeValue");  // Add other headers as needed
                 })
