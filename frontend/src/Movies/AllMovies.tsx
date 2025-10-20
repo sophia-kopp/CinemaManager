ff045839b3e42aef2a5aca3a48b661c29c2abb90
sophiakopp <sophiakopp@gmx.de>
2025-10-20 13:04:48 +0200

    add front end pagination

--- frontend/src/Movies/AllMovies.tsx
+++ frontend/src/Movies/AllMovies.tsx
@@ -1,21 +1,18 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import type {Movie} from "../model/Movie.ts";
-import axios from "axios";
 import MovieCard from "./MovieCard/MovieCard.tsx";
+import MoviePagination from "./MoviePagination/MoviePagination.tsx";
 
 export default function AllMovies(){
     const [movies, setMovies] = useState<Movie[]>([]);
 
-    function loadAllMovies(){
-        axios.get("api/movies")
-            .then(r=> setMovies(r.data))
-            .catch(e=> console.log(e))
-    }
-
+    useEffect(() => {
+        setMovies(movies)
+    }, [movies]);
     return (
         <>
             <h3>All Movies</h3>
-            <button onClick={loadAllMovies}>Go Get Them!</button>
+            <MoviePagination setMovies={setMovies}/>
             {movies.map(m=>
                 <MovieCard movie={m}/>
             )}
---
+++ frontend/src/Movies/MoviePagination/MoviePagination.tsx
@@ -0,0 +1,58 @@
+import {useEffect, useState} from "react";
+import type {Movie} from "../../model/Movie.ts";
+import axios from "axios";
+
+type MoviePaginationProps = {
+    setMovies: (movies: Movie[]) => void;
+}
+
+export default function MoviePagination(props: Readonly<MoviePaginationProps>) {
+    const [moviesLoaded, setMoviesLoaded] = useState<boolean>(false);
+    const [pageIndex, setPageIndex] = useState<number>(1);
+
+    function loadAllMovies() {
+      loadPage()
+    }
+
+    function getAnteriorPage() {
+        if (pageIndex <= 1) {
+            setPageIndex(1);
+        } else{
+            setPageIndex(pageIndex - 1);
+        }
+    }
+
+    function getNextPage() {
+        if (pageIndex >= 500) {
+            setPageIndex(500);
+        } else {
+            setPageIndex(pageIndex + 1);
+        }
+    }
+
+    function loadPage() {
+        axios.get("api/movies/" + pageIndex)
+            .then(r => {
+                //setMovies(r.data)
+                props.setMovies(r.data)
+                setMoviesLoaded(true)
+            })
+            .catch(e => console.log(e))
+    }
+
+    useEffect(() => {
+        loadPage()
+    }, [pageIndex]);
+
+
+    return (
+        <div className={"pagination-buttons"}>
+            {!moviesLoaded && <button onClick={loadAllMovies}>Go Get Them!</button>}
+            {moviesLoaded && <button onClick={getAnteriorPage}>Anterior Page</button>}
+            {moviesLoaded && <p>{pageIndex}</p>}
+            {moviesLoaded && <button onClick={getNextPage}>Next Page</button>}
+
+
+        </div>
+    )
+}
