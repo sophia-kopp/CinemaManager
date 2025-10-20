1e122483b7c208fb5e98f6d4fbf0382dff852013
sophiakopp <sophiakopp@gmx.de>
2025-10-20 11:47:27 +0200

    add test for service with mock rest service server

--- backend/src/test/java/org/example/backend/service/MovieServiceTest.java
+++ backend/src/test/java/org/example/backend/service/MovieServiceTest.java
@@ -1,52 +1,56 @@
 package org.example.backend.service;
 
-import org.example.backend.model.movie.Movie;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureMockRestServiceServer;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.web.client.RestClient;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
-import java.util.ArrayList;
-import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
+@AutoConfigureMockMvc
+@AutoConfigureMockRestServiceServer
 class MovieServiceTest {
-    private RestClient.Builder restClient;
-    private MovieService service;
 
-    @BeforeEach
-    void setup() {
-        restClient = mock(RestClient.Builder.class);
-        service = new MovieService(restClient);
-    }
+    private final RestClient.Builder restClient = RestClient.builder();
+    private final MockRestServiceServer server = MockRestServiceServer.bindTo(restClient).build();
 
     @Test
     void getAllMovies() {
+        server.expect(requestTo("https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1"))
+                .andExpect(method(HttpMethod.GET))
+                .andRespond(withSuccess("""  
+                        {
+                          "page": 1,
+                          "results": [
+                                {
+                                "adult": false,
+                                        "backdrop_path": "/7QirCB1o80NEFpQGlQRZerZbQEp.jpg",
+                                        "genre_ids": [
+                                            10749,
+                                            18
+                                        ],
+                                        "id": 1156594,
+                                        "original_language": "es",
+                                        "original_title": "Culpa nuestra",
+                                        "overview": "Jenna and Lion's wedding brings about the long-awaited reunion between Noah and Nick after their breakup. Nick's inability to forgive Noah stands as an insurmountable barrier. He, heir to his grandfather's businesses, and she, starting her professional life, resist fueling a flame that's still alive. But now that their paths have crossed again, will love be stronger than resentment?",
+                                        "popularity": 1162.7582,
+                                        "poster_path": "/yzqHt4m1SeY9FbPrfZ0C2Hi9x1s.jpg",
+                                        "release_date": "2025-10-15",
+                                        "title": "Our Fault",
+                                        "video": false,
+                                        "vote_average": 7.942,
+                                        "vote_count": 52
+                                    }],
+                          "total_pages": 38020,
+                          "total_results": 760385}
+                        """, MediaType.APPLICATION_JSON
+                ));
 
-        Movie movie = new Movie(false,
-                "/8YFL5QQVPy3AgrEQxNYVSgiPEbe.jpg",
-               List.of(28,
-                       12,
-                       878),
-                640146,
-                "en",
-                "Ant-Man and the Wasp: Quantumania",
-                "Super-Hero partners Scott Lang and Hope van Dyne, along with with Hope's parents Janet van Dyne and Hank Pym, and Scott's daughter Cassie Lang, find themselves exploring the Quantum Realm, interacting with strange new creatures and embarking on an adventure that will push them beyond the limits of what they thought possible.",
-                9272.643,
-                "/ngl2FKBlU4fhbdsrtdom9LVLBXw.jpg",
-                "2023-02-15",
-                "Ant-Man and the Wasp: Quantumania",
-                false,
-                6.5,
-                1856);
 
-        List<Movie> moviesList = new ArrayList<>();
-        moviesList.add(movie);
-
-        when(restClient.build().get().retrieve()).thenReturn((RestClient.ResponseSpec) moviesList);
-        List<Movie> movies = service.getAllMovies();
-        assertFalse(movies.isEmpty());
     }
 }
