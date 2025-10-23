b300a91ad13d03702f89aa5afc9b4dd7bf5c1624
sophiakopp <sophiakopp@gmx.de>
2025-10-23 11:15:22 +0200

    add tests for fav movies

---
+++ backend/src/test/java/org/example/backend/controller/FavouriteMovieControllerTest.java
@@ -0,0 +1,74 @@
+package org.example.backend.controller;
+
+import org.example.backend.model.movie.FavouriteMovie;
+import org.example.backend.repo.FavouriteMovieRepo;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class FavouriteMovieControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private FavouriteMovieRepo repo;
+
+    @BeforeEach
+    void setup() {
+        repo.deleteAll();
+    }
+
+    @Test
+    void getAllFavouriteMovie_ShouldReturnListOfFavMovies_WhenGetAllIsCalled() throws Exception {
+
+        FavouriteMovie favMovie = new FavouriteMovie("1", "test1");
+        repo.save(favMovie);
+        //when
+        mockMvc.perform(get("/api/favouriteMovies"))
+                //then
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(
+                        """
+                                [
+                                {"id":  "1",
+                                "name": "test1"
+                                }
+                                ]
+                                
+                                """
+                ));
+
+    }
+
+    @Test
+    void addNewFavouriteMovie_ShouldReturnFavMovie_WhenFavMovieIsAdded() throws Exception {
+        mockMvc.perform(post("/api/favouriteMovies")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(
+                                """
+                                        {
+                                        "id": "1",
+                                        "name": "test1"
+                                        }
+                                        """
+                        ))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(
+                        """
+                                {"id":  "1",
+                                "name": "test1"}
+                                """
+                ));
+    }
+}
