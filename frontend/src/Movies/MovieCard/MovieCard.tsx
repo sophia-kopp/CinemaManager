0a77f23e5bbfa39d5559cd6935c493c7bdda7075
sophiakopp <sophiakopp@gmx.de>
2025-10-23 09:53:58 +0200

    create frontend

--- frontend/src/Movies/MovieCard/MovieCard.tsx
+++ frontend/src/Movies/MovieCard/MovieCard.tsx
@@ -1,12 +1,21 @@
 import type {Movie} from "../../model/Movie.ts";
 import './MovieCard.css'
+import axios from "axios";
 
 type MovieCardProps={
     movie: Movie
 }
 export default function MovieCard(props: Readonly<MovieCardProps>){
+
+   function onAddToFavourite(){
+       axios.post("api/favouriteMovies")
+           .then(r=>console.log(r.data))
+           .catch(e=>console.log(e));
+   }
+
     return (
         <div className={"movie-card"}>
+            <button onClick={onAddToFavourite}>Favourite</button>
             <img src={props.movie.poster_path} alt={"movie-img"}/>
             <h4>{props.movie.title}</h4>
             <p>Release date: {props.movie.release_date}</p>
