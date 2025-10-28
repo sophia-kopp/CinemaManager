09616483b64199ee1a674453cebcc69a8cbc5de6
sophiakopp <sophiakopp@gmx.de>
2025-10-28 14:25:55 +0100

    modify presentation card

--- frontend/src/Schedule/PresentationCard/PresentationCard.tsx
+++ frontend/src/Schedule/PresentationCard/PresentationCard.tsx
@@ -1,6 +1,7 @@
 import type {Presentation} from "../../model/Presentation.ts";
 import axios from "axios";
 import {useNavigate} from "react-router-dom";
+import {useEffect, useState} from "react";
 
 type PresentationCardProps = {
     presentation: Presentation
@@ -10,6 +11,7 @@ type PresentationCardProps = {
 export default function PresentationCard(props: Readonly<PresentationCardProps>) {
 
     const nav= useNavigate();
+    const [date, setDate] = useState<string>("");
 
     function deletePresentation() {
         axios.delete("api/presentations/" + props.presentation.id)
@@ -20,11 +22,20 @@ export default function PresentationCard(props: Readonly<PresentationCardProps>)
         nav("/editPresentation/" + props.presentation.id);
     }
 
+function calculateEndsAt(){
+        setDate((props.presentation.startsAt.toString().substring(0, 10)));
+}
+
+    useEffect(() => {
+        calculateEndsAt()
+    }, []);
 
     return (
         <div>
             <p>Movie: {props.presentation.movieName}</p>
+            <p>Day: {date}</p>
             <p>Starts at: {props.presentation.startsAt}</p>
+            <p>Ends at: {props.presentation.startsAt}</p>
             <p>Duration: {props.presentation.duration}</p>
             <button onClick={deletePresentation}>Delete</button>
             <button onClick={editPresentation}>Edit</button>
