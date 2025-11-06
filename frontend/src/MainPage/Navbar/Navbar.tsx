b38c652be6aa5e421616c688ef6955606595c8ce
sophiakopp <sophiakopp@gmx.de>
2025-11-06 15:43:56 +0100

    add logout

--- frontend/src/MainPage/Navbar/Navbar.tsx
+++ frontend/src/MainPage/Navbar/Navbar.tsx
@@ -2,6 +2,16 @@ import {Link} from "react-router-dom";
 import './Navbar.css';
 
 export default function Navbar(){
+
+    function logout(){
+
+            const host: string = window.location.host === "localhost:5173" ?
+                "http://localhost:8080"
+                :
+                window.location.origin;
+            window.open(host + "/logout", "_self")
+    }
+
     return (
         <div className={"navbar"}>
             <Link to={"/"} className={"nav-button"}>Home</Link>
@@ -9,6 +19,7 @@ export default function Navbar(){
             <Link to={"/allMovies"} className={"nav-button"}>All Movies</Link>
             <Link to={"/allFavMovies"} className={"nav-button"}>Favourite Movies</Link>
             <Link to={"/allHalls"} className={"nav-button"}>Halls</Link>
+            <button onClick={logout}>Logout</button>
         </div>
     )
 }
