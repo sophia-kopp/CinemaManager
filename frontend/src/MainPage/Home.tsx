36dce54ff49014dc6fa3e3b9f45daa4454dbaa4d
sophiakopp <sophiakopp@gmx.de>
2025-11-06 15:43:38 +0100

    add login and protected routes

--- frontend/src/MainPage/Home.tsx
+++ frontend/src/MainPage/Home.tsx
@@ -1,29 +1,13 @@
-import axios from "axios";
-import {useEffect} from "react";
-export default function Home() {
+type HomeProps = {
+    user: string
+}
 
-    function login() {
-        const host: string = window.location.host === "localhost:5173" ?
-            "http://localhost:8080"
-            :
-            window.location.origin;
-        window.open(host + "/oauth2/authorization/github", "_self")
-    }
+export default function Home(props:Readonly<HomeProps>) {
 
-    const loadUser = ()=>{
-        axios.get("api/auth/me")
-            .then(r=>console.log(r.data))
-            .catch(e=> console.log(e));
-    }
 
-    useEffect(() => {
-        loadUser()
-    }, []);
     return (
         <>
-            <h2>Welcome!</h2>
-            <p>Please log in:</p>
-            <button onClick={login}>Login</button>
+            <h2>Welcome {props.user}!</h2>
         </>
     )
 }
