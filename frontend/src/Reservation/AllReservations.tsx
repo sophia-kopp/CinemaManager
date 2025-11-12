8bd3d164dedfb45cf1dce450102fe6c4e366cc3b
sophiakopp <sophiakopp@gmx.de>
2025-11-12 14:25:35 +0100

    button copy id to clipboard

--- frontend/src/Reservation/AllReservations.tsx
+++ frontend/src/Reservation/AllReservations.tsx
@@ -2,7 +2,7 @@ import {useEffect, useState} from "react";
 import type {Reservation} from "../model/Reservation.ts";
 import axios from "axios";
 import "./AllReservations.css";
-import { GiPerson } from "react-icons/gi";
+import {GiPerson} from "react-icons/gi";
 
 export default function AllReservations() {
 
@@ -14,6 +14,21 @@ export default function AllReservations() {
             .catch(e => console.log(e))
     }
 
+    const [isCopied, setIsCopied] = useState(false);
+
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            setIsCopied(false);
+        }, 2000);
+        return () => clearTimeout(timeoutId);
+    }, [copyToClipboard]);
+
+    function copyToClipboard(id: string) {
+        navigator.clipboard.writeText(id);
+        console.log("Copied ID to Clipboard: ", id)
+        setIsCopied(true);
+    }
+
     useEffect(() => {
         loadAllReservations();
     }, []);
@@ -23,18 +38,20 @@ export default function AllReservations() {
             <h3>All Reservations</h3>
             {reservations.map(reservation =>
                 <div className={"reservation"}>
-                    <p>Id: {reservation.id}</p>
-                    <h4>{reservation.presentation.movieName}</h4>
+                    <div className={"reservation-header"}>
+                        <h4>{reservation.presentation.movieName}</h4>
+                        {!isCopied && <button onClick={() => copyToClipboard(reservation.id)}>📎 Copy ID</button>}
+                        {isCopied && <button className={"button-copied"}>✓ Copied!</button>}
+                    </div>
                     <p>Price: {reservation.price} $</p>
-                    <p>Date: {reservation.presentation.startsAt.toString().substring(0,10)} -
-                        Starts at: {reservation.presentation.startsAt.toString().substring(11,16)}</p>
-                    <p><GiPerson /> {reservation.amountOfSeats} Person(s)</p>
+                    <p>Date: {reservation.presentation.startsAt.toString().substring(0, 10)} -
+                        Starts at: {reservation.presentation.startsAt.toString().substring(11, 16)}</p>
+                    <p><GiPerson/> {reservation.amountOfSeats} Person(s)</p>
                     <p>Seats:</p>
                     {reservation.seatPositions.map(seat =>
-                         <p>Row: {seat.row.toUpperCase()} - Seat: {seat.seatNumber}</p>
+                        <p>Row: {seat.row.toUpperCase()} - Seat: {seat.seatNumber}</p>
                     )}
                 </div>
-
             )}
         </>
     )
