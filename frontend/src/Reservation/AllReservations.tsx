56aa29bb7d8088e05a863e360e51299174f05478
sophiakopp <sophiakopp@gmx.de>
2025-11-12 13:58:25 +0100

    add styling to all reservations

--- frontend/src/Reservation/AllReservations.tsx
+++ frontend/src/Reservation/AllReservations.tsx
@@ -1,6 +1,8 @@
 import {useEffect, useState} from "react";
 import type {Reservation} from "../model/Reservation.ts";
 import axios from "axios";
+import "./AllReservations.css";
+import { GiPerson } from "react-icons/gi";
 
 export default function AllReservations() {
 
@@ -20,14 +22,16 @@ export default function AllReservations() {
         <>
             <h3>All Reservations</h3>
             {reservations.map(reservation =>
-                <div>
+                <div className={"reservation"}>
                     <p>Id: {reservation.id}</p>
-                    <p>MovieName: {reservation.presentation.movieName}</p>
-                    <p>Starts at: {reservation.presentation.startsAt.toString()}</p>
-                    <p>Amount of Seats: {reservation.amountOfSeats}</p>
-                    <p>Price: {reservation.price}</p>
-                     {reservation.seatPositions.map(seat =>
-                         <p>Row: {seat.row} - Seat: {seat.seatNumber}</p>
+                    <h4>{reservation.presentation.movieName}</h4>
+                    <p>Price: {reservation.price} $</p>
+                    <p>Date: {reservation.presentation.startsAt.toString().substring(0,10)} -
+                        Starts at: {reservation.presentation.startsAt.toString().substring(11,16)}</p>
+                    <p><GiPerson /> {reservation.amountOfSeats} Person(s)</p>
+                    <p>Seats:</p>
+                    {reservation.seatPositions.map(seat =>
+                         <p>Row: {seat.row.toUpperCase()} - Seat: {seat.seatNumber}</p>
                     )}
                 </div>
 
